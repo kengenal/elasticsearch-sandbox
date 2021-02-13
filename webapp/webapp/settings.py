b58bbcfb99c12f72f60a6c9475868754ce5fcc8b
kengenal <kengenal1@gmail.com>
2021-02-13 13:34:20 +0100

    Change database location and create pypi_package model

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ROOT_DIR / 'database/db.sqlite3',
     }
 }
 
